c719ae4c8bfa99dcb9384679857b072e7a8d4aad
Luis Canada <canadaluis5@gmail.com>
2026-02-18 22:05:13 -0500

    Add initial web app scaffold for PWA rollout

---
+++ packages/web-app/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-8">
+      <h1 className="text-2xl font-semibold">MDX Web</h1>
+    </main>
+  );
+}
+
+export default App;
